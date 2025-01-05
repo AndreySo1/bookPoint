d6136de5945120dd2ea70edeb8549e9b1af394ea
andrey.soroko <andrey.soroko@neotech.by>
2025-01-05 21:08:04 +0300

    fix: add entity end properties for orm

---
+++ src/main/java/by/tms/bookpoint/entity/Staff.java
@@ -0,0 +1,21 @@
+package by.tms.bookpoint.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Staff {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String staff_id;
+    private String name;
+    private String surname;
+    private String password;
+}
